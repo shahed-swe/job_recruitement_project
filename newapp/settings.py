483d096d4b8e389579564898e2f00ed278f055ae
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-06-03 01:38:58 +0600

    changing settings for hiding my email and pass

--- newapp/settings.py
+++ newapp/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = "tafsirameri51@gmail.com"
-EMAIL_HOST_PASSWORD = "qldqjqiqfmjvwyrd"
+EMAIL_HOST_USER = "your email"
+EMAIL_HOST_PASSWORD = "your app password"
 EMAIL_USE_TLS = True
 
 
