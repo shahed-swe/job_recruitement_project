457fd9ef6511180c158b24ecba6e31be1195c096
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-06-02 22:10:50 +0600

    testing response into country url with every method

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -24,7 +24,7 @@ class BasicCountryTestCase(APITestCase):
         pass
 
 
-class TestDemo(BasicCountryTestCase):
+class TestCountryDemo(BasicCountryTestCase):
 
     def test_zero_country_list(self)->None:
         response = self.client.get(self.country_url)
@@ -46,6 +46,13 @@ class TestDemo(BasicCountryTestCase):
         }
         response = self.client.post(self.country_url, body)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json.loads(response.content),{
+            "id":1,
+            "name":"Bangladesh",
+            "latitude":56.64567,
+            "longitude":45.67,
+            "code":"880"}
+            )
 
 
     def test_every_columns_needs_to_register_two(self)->None:
@@ -68,11 +75,44 @@ class TestDemo(BasicCountryTestCase):
         }
         response = self.client.put(self.country_url+f"{country.pk}/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),{
+            "id":country.pk,
+            "name": "India",
+            "latitude":56.6768,
+            "longitude":78.3245,
+            "code":"91"
+        })
 
     def test_update_individual_column(self)->None:
-        country = Country.objects.create(name="Bangladesh",latitude="45.1241341", longitude="47.1234",code="880")
+        country = Country.objects.create(name="India",latitude="45.1241341", longitude="47.1234",code="880")
         data = {
             "name":"Bangladesh",
         }
         response = self.client.patch(self.country_url+f"{country.pk}/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),{"id":country.pk,"name":"Bangladesh","latitude":45.1241341,"longitude":47.1234,"code":"880"})
+
+    def test_delete_data(self)->None:
+        country = Country.objects.create(name="India",latitude="45.1241341", longitude="47.1234",code="880")
+        response = self.client.delete(self.country_url+f"{country.pk}/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        print(response.content)
+
+
+
+@pytest.mark.django_db
+class BasicStateTestCase(APITestCase):
+    def setUp(self)->None:
+        self.state_url = reverse('states-list')
+        self.user = User.objects.create_user(email="testmain@gmail.com", first_name="test", last_name="last",password="hello@123")
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.token.access_token}")
+    
+    def tearDown(self) -> None:
+        pass
+
+class TestStateDemo(BasicStateTestCase):
+    def test_zero_state_list(self)->None:
+        response = self.client.get(self.state_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content), [])
