4230166a1fd49c97d466064d4c62e76f59d689e1
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-06-17 12:58:51 +0600

    changing fail safe

--- newapp/settings.py
+++ newapp/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/static'
+STATIC_ROOT = BASE_DIR + 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
