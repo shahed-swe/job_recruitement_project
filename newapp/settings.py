093abb4be4abb8ca31c24486cbe6c1559b363382
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-06-17 20:24:47 +0600

    adding path for media and static

--- newapp/settings.py
+++ newapp/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = BASE_DIR / 'static'
-
+MEDIA_URL= '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
